8adf141b24183715e08361a27be6171b61946c93
AdwaitAdk <adwaitadk@tuta.io>
2022-03-16 14:35:27 +0545

    Finished the merge technique

--- merge.c
+++ merge.c
@@ -1,45 +1,62 @@
-// Merging two sorted array to a single array
+// Arranging an array with two sorted sections
 
 #include <stdio.h>
 
 void merge(int arr[],int l,int mid,int r)
 {
+  // n1 is the length of arr1. The first array should be of the length up to and including the mid of the original array.
+  int n1 = mid;
+  // n2 is the length of arr2. The second array should be of the length from the mid of the original array to the last.
+  int n2 = r-(mid+1);
 
-  int arr1[mid];
-  int arr2[r-(mid+1)];
-  
+  int arr1[n1];
+  int arr2[n2];
 
-  for (int i = l; i <= mid; i++) {
+  // Adding values from the original array up to and including the mid point.
+  for (int i = l; i <= n1; i++) {
     arr1[i] = arr[i]; 
   }
 
-  for (int i = mid+1; i <= r; i++) {
-    arr2[i-(mid+1)] = arr[i]; 
+  // Adding values from the original array starting from mid point and up to the last.
+  for (int i = 0; i <= n2; i++) {
+    arr2[i] = arr[mid+1+i]; 
   }
 
+  // Initializing pointer variables. Here i,j and k points to the index to arr1, arr2 and arr respectively.
   int i, j, k;
   i = j = k = 0;
 
-  while (i < 4 && j < 4) {
+  // Checking the values in the two arrays namely arr1 and arr2.
+  while (i <= n1 && j <= n2) {
+  // If the value in i position of arr1 is smaller than value in j position of arr2, add that value to the k position of arr array.
     if (arr1[i] < arr2[j]) {
       arr[k] = arr1[i];
       k++; i++;
-    } else {
+    }
+  // Else if the value in j position of arr2 is smaller than value in i position of arr1, add that value to the k position off arr array.
+    else if (arr2[j] < arr1[i]) {
       arr[k] = arr2[j];
       k++; j++;
     }
+  // Else if both the values are equal, add both the values to arr array in position k and k+1 and increase the k pointer by 2 and increase the pointer of both arr1 and arr2.
+    else {
+      arr[k] = arr1[i];
+      arr[k+1] = arr2[j];
+      k += 2; i++; j++;
+    }
   }
-
-  while(i < 4){
+ 
+  // Since && operator was used in the while loop, one of the values either i or j will not have reached the end of the arr1 or arr2 respectively.
+  // For that the following two loops are used.
+  while(i <= n1){
     arr[k]= arr1[i];
     k++; i++;
   }
-
-  while(j < 4){
+  
+  while(j <= n2){
     arr[k]= arr2[j];
     k++; j++;
   }
-
 }
 
 int main (int argc, char *argv[])
@@ -50,10 +67,11 @@ int main (int argc, char *argv[])
   int l = 0, r = 7, mid;
   mid = (l+r)/2;
 
-  merge(arr, l, r, mid);
+  merge(arr, l, mid, r);
 
   for (int i = 0; i <= 7; i++) {
     printf("%d\n", arr[i]);
   }
+
   return 0;
 }
