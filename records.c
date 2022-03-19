e9e91c7e788bce83d7156f4d7be48690e61ea9c4
AdwaitAdk <adwaitadk@tuta.io>
2022-03-19 12:23:08 +0545

    added mergeSort algorithm

--- records.c
+++ records.c
@@ -3,27 +3,93 @@
 #include <stdio.h>
 
 struct student {
+  char name[20];
   int sid;
   int height;
-  char name[];
 };
 
+void merge(struct student arr[], int l, int mid, int r)
+{
+  int n1 = mid - l + 1; // Index of first temp array
+  int n2 = r - mid; // Index of second temp array
+
+  // Temp arrays
+  struct student arr1[n1];
+  struct student arr2[n2];
+
+  // Filling temp array with values from parent array
+  for (int i = 0; i < n1; i++) {
+    arr1[i] = arr[i+l];
+  }
+
+  for (int i = 0; i < n2; i++) {
+    arr2[i] = arr[mid + 1 + i];
+  }
+
+  int i, j, k;
+  i = 0; // Pointer of first temp array
+  j = 0; // Pointer of second temp array
+  k = l; // Pointer of parent array
+
+  // Checking values within the temp arrays
+  while (i < n1 && j < n2) {
+    if(arr1[i].height < arr2[j].height){
+      i++;
+    } else {
+      arr[k] = arr2[j];
+      j++;
+    }
+    k++;
+  }
+
+  // Filling the remaining vales from the temp arrays
+  while (i < n1) {
+    arr[k] = arr1[i];
+    k++; i++;
+  }
+
+  while (j < n2) {
+    arr[k] = arr2[j];
+    k++; j++;
+  }
+}
+
+// l is the left index value and r is the right index value
+void mergeSort(struct student arr[], int l, int r)
+{
+  if (l < r){
+    int mid = (l+r)/2;
+
+    // Recursive function call for sorting the array
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid+1, r);
+    
+    merge(arr, l, mid, r);
+  }
+}
 int main() {
-  struct student std[100], std2[100];
+  struct student std[100];
   int records;
 
   printf("Enter number of records: ");
   scanf("%d", &records);
 
-  for (int i = 1; i <= records; i++) {
-    printf("Enter sid, name and height of student: ");
-    scanf("%d %s %d", &std[i].sid, &std[i].name, &std[i].height);
+  for (int i = 0; i < records; i++) {
+    printf("Enter name of student: ");
+    scanf("%s", std[i].name);
+    printf("Enter sid of student: ");
+    scanf("%d", &std[i].sid);
+    printf("Enter height of student: ");
+    scanf("%d", &std[i].height);
   }
+  
+  mergeSort(std, 0, records-1);
 
+  printf("The list of students is:\n");
+  printf("Name\tSID\tHeight\n");
 
-  printf("The list of students is: ");
-  for (int i = 1; i <= records; i++) {
-    printf("%d %s %d", std[i].sid, std[i].name, std[i].height);
+  for (int i = 0; i < records; i++) {
+    printf("%s\t%d\t%d\n", std[i].name, std[i].sid, std[i].height);
   }
 
   return 0;
